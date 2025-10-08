5722f50f1b23ac04b4448a1d017d0c396e47e346
Potnuru Karthik <2320040019@klh.edu.in>
2025-10-08 19:13:38 +0530

    Update Ene&Pow.m

--- Energy-Power/Ene&Pow.m
+++ Energy-Power/Ene&Pow.m
@@ -6,7 +6,7 @@ close all;
 x1=[0 1 2 2 2];
 Ene=sum(abs(x1).^2);
 
-subplot(1,3,1);
+subplot(1,2,1);
 stem(0:length(x1)-1,x1,'filled','LineWidth',1.5);
 title(['Energy = ', num2str(Ene)]);
 xlabel('n --->');
@@ -18,22 +18,12 @@ x2=[1 2 2 2];
 N1=length(x2);
 Power1=(1/N1)*sum(abs(x2).^2);
 
-subplot(1,3,2);
+%or power for the same x1 would be Power1=(1/N1)*Ene;
+
+subplot(1,2,2);
 stem(0:length(x2)-1, x2, 'filled', 'LineWidth', 1.5);
 title(['Power = ',num2str(Power1)]);
 xlabel('n --->');
 ylabel('Amplitude');
 grid on;
 
-% Signal 3: Periodic Power Signal
-n=0:4;
-x3=2*(-1).^n;
-N2=length(x3);
-Power2=(1/N2)*sum(abs(x3).^2);
-
-subplot(1,3,3);
-stem(n,x3,'filled','LineWidth',1.5);
-title(['Power = ',num2str(Power2)]);
-xlabel('n --->');
-ylabel('Amplitude');
-grid on;
